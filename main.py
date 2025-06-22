2fd27898d58667f3de6b2e902aa315c2317404ae
Bikita <bikitasuwal12@gmail.com>
2025-06-22 18:45:29 +0545

    Added expense function

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 #Expense tracker project
 
+def add_expense():
+    name = input("Enter expense name: ")
+    category = input("Enter category: ")
+    amount = input("Enter amount: ")
+    with open("expenses.txt", "a") as f:
+        f.write(f"{name},{category},{amount}\n")
